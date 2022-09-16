b8044245008791e309dde6a36695810da7b888c2
unknown <eodrmfdl1004@naver.com>
2022-09-16 14:37:58 +0900

    create habit-tracker vue.js

---
+++ habit-tracker/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Main from './views/Main.vue';
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style scoped>
+
+</style>
---
+++ habit-tracker/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <font-awesome-icon icon="fa-solid fa-leaf" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+
+</style>

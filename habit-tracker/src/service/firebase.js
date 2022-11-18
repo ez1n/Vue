91f6a4f92830207ed745ba323ae7f999a397f390
unknown <eodrmfdl1004@naver.com>
2022-11-18 23:06:38 +0900

    refactor: add firebase base url

--- habit-tracker/src/service/firebase.js
+++ habit-tracker/src/service/firebase.js
@@ -9,6 +9,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DB_URL,
   projectId: import.meta.env.VITE_PRJ_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   appId: import.meta.env.VITE_ID,

ae516f9b0aa5727ecc3d095088fae88642525cf2
unknown <eodrmfdl1004@naver.com>
2022-11-21 18:32:28 +0900

    docs: 함수 주석 추가

--- habit-tracker/src/service/firebase.js
+++ habit-tracker/src/service/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import { getDatabase } from "firebase/database";
+import {getDatabase} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-export const database = getDatabase(firebaseApp);
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
 
-export default {firebaseApp, database};
+export {database};
--- habit-tracker/src/service/habit_repository.js
+++ habit-tracker/src/service/habit_repository.js
@@ -2,6 +2,7 @@ import {database} from "@/service/firebase";
 import { ref, set, remove, update, onValue } from "firebase/database";
 
 class HabitRepository {
+  /* get habits */
   static getHabits(setHabit) {
     onValue(ref(database,`habits`), (habits) => {
       if (habits.exists()) {
@@ -12,15 +13,17 @@ class HabitRepository {
     })
   }
 
+  /* habit 추가 */
   static addHabit(habit) {
     return set(ref(database, `habits/${habit.id}`), habit)
   }
-
+  /* habit 삭제 */
   static deleteHabit(id) {
     const habitRef = ref(database, `habits/${id}`);
     return remove(habitRef);
   }
 
+  /* habit 증가, 감소 */
   static countHabit(id, habit) {
     const habitRef = ref(database, `habits/${id}`);
     return update(habitRef, habit);
